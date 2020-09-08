94c10274d15a29846c8a87eea11ad5b6709940f6
Pavel Ivanov <p200300@mail.ru>
2020-09-08 11:42:33 +0300

    change format font .ttf ti .woff make 1st screen og 'METR'

--- resources/views/METR.blade.php
+++ resources/views/METR.blade.php
@@ -12,7 +12,27 @@
 </head>
 <body>
 <div id="METR">
-
+    <section class="main">
+        <div class="header">
+            <div class="header_logo">
+                <img src="{{ asset('img/logo/metr.svg') }}" alt="">
+            </div>
+            <div class="header_burger"></div>
+            <div class="header_navbar">
+                <a href="#" class="M_btn-main header_navbar-link"><span>о компании</span></a>
+                <a href="#" class="M_btn-main header_navbar-link"><span>портфолио</span></a>
+                <a href="#" class="M_btn-main header_navbar-link"><span>цены</span></a>
+                <a href="#" class="M_btn-main header_navbar-link"><span>отзывы</span></a>
+                <a href="#" class="M_btn-main header_navbar-link"><span>контакты</span></a>
+            </div>
+        </div>
+        <div class="M_H1 main_text">Студия дизайна лучших интерьеров</div>
+        <a href="#" class="M_btn-order main_order">Заказать проект</a>
+        <div class="main_social">
+            <div class="M_btn-icon M_btn-icon-instagramm main_social-icon"></div>
+            <div class="M_btn-icon M_btn-icon-vkontakte main_social-icon"></div>
+        </div>
+    </section>
 </div>
 </body>
 </html>
